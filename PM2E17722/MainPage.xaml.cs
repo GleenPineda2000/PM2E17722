392b5ab117e437c04424c909ee50902aa118cce3
GleenPineda2000 <gleenpineda2000@gmail.com>
2023-06-15 01:42:05 -0600

    Agregar archivos de proyecto.

---
+++ PM2E17722/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2E17722
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        Plugin.Media.Abstractions.MediaFile photo;
+
+
+        bool resul = false;
+
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+          
+        }
+
+        private async void btnfoto_Clicked(object sender, EventArgs e)
+        {
+
+            photo = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "imagen",
+                Name = "Foto.jpg",
+                SaveToAlbum = true
+            });
+
+            if (photo != null)
+            {
+                foto.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+            }
+
+
+
+
+        }
+
+        private async void btnmapa_Clicked(object sender, EventArgs e)
+        {
+
+
+            resul = await DisplayAlert("TERMINOS DE USO", "¿TIENE ENCENDIDO SU GPS?", "SI", "NO");
+
+
+
+
+
+            if (resul == true)
+            {
+
+                await Navigation.PushAsync(new PageMapa());
+
+
+            }
+            else
+            {
+
+                await DisplayAlert("ERROR", "SE NECESITA ENCENDER EL GPS PARA GEOLOCALIZACION","CERRAR");
+
+
+
+            }
+           
+
+         
+
+       
+
+        }
+    }
+}
